edeaa43662b9a6f85a2072872716591d58a5dc99
Pigan-Et <819287763@qq.com>
2026-03-09 18:03:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 // 数据索引对应关系：
 // 0:title, 1:variant, 2:price, 3:imgUrl, 4:link, 5:l1, 6:l2, 7:inviteId, 8:modelId, 9:isFull
 const products = [
-["Dunlop ScootSmart2 摩托车轮胎适用于 Forza350 / Xmax / Nmax / Aerox / PCX160 / Adv / Zontes 350E，轮辋尺寸为 13\"-15\"。","一对 PCX160 F + R，25 年款。","290","https://cf.shopee.co.th/file/th-11134207-7ra0q-mbvx0csm22xf85","http://shopee.co.th/product/225387673/5463743068","摩托车零配件","轮胎及配件","IVCN2025111458238","","否"]
+["Dunlop ScootSmart2 摩托车轮胎轮辋尺寸为 13\"-15\"。","一对 PCX160 F + R，25 年款。","290","https://cf.shopee.co.th/file/th-11134207-7ra0q-mbvx0csm22xf85","http://shopee.co.th/product/225387673/5463743068","摩托车零配件","轮胎及配件","IVCN2025111458238","","否"]
   // ...这里放你的2万条数据
 
 ];
+
