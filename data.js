9e0e48c965c95068691c738ceb9bf2e10f445f2a
Pigan-Et <819287763@qq.com>
2026-03-09 16:26:00 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+// 你只需要在 [ ] 括号内更新内容
+const products = [
+  {
+  "title" : "BKAH43000000 前大灯，前大灯总成，LED前大灯总成，雅马哈XMAX Connected 2023原装正品",
+  "variant" : "BKAH43000000",
+  "price" : 618,
+  "imgUrl" : "https://cf.shopee.co.th/file/th-11134207-23010-vx4l4d0b6rmvec",
+  "link" : "http://shopee.co.th/product/55852559/18771847480",
+  "l1" : "摩托车零配件",
+  "l2" : "灯光",
+  "inviteId" : "IVCN2026021161247",
+  "modelId" : 117907895578,
+  "isFull" : "否"
+},
+{
+  "title" : "适用于 PCX160（2021-2024 年款）的本田原装大灯。[[33100-K1Z-T01]] #本田原装 #2021 #T01",
+  "variant" : "正品 PCX160 [K1Z-T01]",
+  "price" : 640,
+  "imgUrl" : "https://cf.shopee.co.th/file/th-11134207-81ztk-mi9stmjbhati92",
+  "link" : "http://shopee.co.th/product/86215348/40427250411",
+  "l1" : "摩托车零配件",
+  "l2" : "灯光",
+  "inviteId" : "IVCN2026021161249",
+  "modelId" : 405298951129,
+  "isFull" : "是"
+}
+];
