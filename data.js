707c1a8f06226249c7ddef519009665e377a4205
Pigan-Et <819287763@qq.com>
2026-03-09 17:56:30 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+// 数据索引对应关系：
+// 0:title, 1:variant, 2:price, 3:imgUrl, 4:link, 5:l1, 6:l2, 7:inviteId, 8:modelId, 9:isFull
+const products = [
+["BKAH43000000 前大灯，前大灯总成，LED前大灯总成，雅马哈XMAX Connected 2023原装正品","BKAH43000000","618","https://cf.shopee.co.th/file/th-11134207-23010-vx4l4d0b6rmvec","http://shopee.co.th/product/55852559/18771847480","摩托车零配件","灯光","IVCN2026021161247","117907895578","否"]
+  // ...这里放你的2万条数据
+];
